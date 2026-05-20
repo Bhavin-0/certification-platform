5814daffd94de4769d74879679a58a0e57788ec0
Bhavin-0 <bhavin.joshi@mitaoe.ac.in>
2026-05-20 12:08:32 +0530

    fix: organized pdf files into proper directories

--- src/lib/certificates.ts
+++ src/lib/certificates.ts
@@ -3,22 +3,16 @@ export interface CertificateConfig {
 }
 
 export const certificates: Record<string, CertificateConfig> = {
-  "NPTEL24CS118S105310114304042426": {
-    pdfPath: "/content/noc/NOC24/SEM2/Ecertificates/106/noc24-cs118/Course/NPTEL24CS118S105310114304042426.pdf"
-  },
-  "NPTEL24CS118S105310114504042426": {
-    pdfPath: "/content/noc/NOC24/SEM2/Ecertificates/106/noc24-cs118/Course/NPTEL24CS118S105310114504042426.pdf"
-  },
   "NPTEL24CS118S1053101145": {
     pdfPath: "/content/noc/NOC24/SEM2/Ecertificates/106/noc24-cs118/Course/NPTEL24CS118S1053101145.pdf"
   },
   "NPTEL25DE03S440601127": {
-    pdfPath: "/content/noc/NOC24/SEM2/Ecertificates/106/noc24-cs118/Course/NPTEL25DE03S440601127.pdf"
+    pdfPath: "/content/noc/NOC25/SEM1/Ecertificates/107/noc25-de03/Course/NPTEL25DE03S440601127.pdf"
   },
   "NPTEL25DE01S640601705": {
-    pdfPath: "/content/noc/NOC24/SEM2/Ecertificates/106/noc24-cs118/Course/NPTEL25DE01S640601705.pdf"
+    pdfPath: "/content/noc/NOC25/SEM1/Ecertificates/107/noc25-de03/Course/NPTEL25DE01S640601705.pdf"
   },
   "NPTEL25DE03S340601212": {
-    pdfPath: "/content/noc/NOC25/SEM1/Ecertificates/107/noc25-de03/Course/NPTEL25DE03S34060021201236355.pdf"
+    pdfPath: "/content/noc/NOC25/SEM1/Ecertificates/107/noc25-de03/Course/NPTEL25DE03S340601212.pdf"
   }
 };
